a464f79b7ce4d29e00b83bb228b00a43a2479e79
Simon Engstler <SimonEngstler@live.de>
2023-10-02 13:03:17 +0200

    clear up comments

--- web/src/pages/Group.jsx
+++ web/src/pages/Group.jsx
@@ -18,11 +18,9 @@ const Group = () => {
             }
         };
 
-        // Fetch data when the component is mounted or when groupId changes
         fetchData();
     }, [groupId]);
 
-    // Render the group information
     return (
         <div className="bg-gray-200 h-screen flex flex-col justify-center items-center">
             <div className="bg-white py-8 rounded-lg shadow-md w-screen h-screen">
@@ -33,16 +31,13 @@ const Group = () => {
                     &larr;
                 </button>
                 <h3 className="text-2xl font-semibold text-center mb-6 font-abel">
-                    group.name
+                    {group?.name || 'Loading...'}
                 </h3>
                 <div className="w-screen text-center">
-                    {/* Display group information here */}
-                    {/* Example: */}
                     {group ? (
                         <>
                             <h2 className="text-xl font-semibold">{group.name}</h2>
                             <p>{group.description}</p>
-                            {/* Add more group details as needed */}
                         </>
                     ) : (
                         <p>Loading group information...</p>
